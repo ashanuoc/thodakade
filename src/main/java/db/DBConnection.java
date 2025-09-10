64c45b9bf2b2ed21f1559c5e67b68aec447107aa
ashanuoc <ashanuofl@gmail.com>
2025-09-10 07:04:21 -0400

    add singleton pattern to db connection

---
+++ src/main/java/db/DBConnection.java
@@ -0,0 +1,27 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private Connection connection;
+    private static DBConnection instance;
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_reservation","root","1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+
+        return instance;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
